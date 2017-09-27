9734e1f029a0a7aa0021f4604b32b5788c82b175
Harry Hunt <developingharry@outlook.com>
2017-09-27 21:12:56 +0100

    early code linting, movement refined

--- js/app.js
+++ js/app.js
@@ -1,72 +1,81 @@
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+var Enemy = function (x, y, speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
-};
+  };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+  };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-var Player = function(x, y, speed) {
+var Hero = function (x, y, speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
     this.sprite = 'images/char-boy.png';
-};
+  };
 
-Player.prototype.update = function() {
-//to be continued
-};
+Hero.prototype.update = function () {
+    // something in here
+  };
 
-Player.prototype.render = function() {
+Hero.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  };
 
-Player.prototype.handleInput = function(direction) {
-    switch(direction) {
-      case 'up':
-        this.y -= this.speed;
-        break;
-      case 'down':
-        this.y += this.speed;
-        break;
+Hero.prototype.handleInput = function (direction) {
+    // something in here
+    switch (direction) {
       case 'left':
         this.x -= this.speed;
         break;
       case 'right':
         this.x += this.speed;
         break;
-      };
-    console.log(direction + this.x + this.y);
-};
+      case 'up':
+        this.y -= this.speed - 15;
+        break;
+      case 'down':
+        this.y += this.speed - 15;
+        break;
+      case 'b':
+        console.log('x is ' + this.x + ', y is ' + this.y + '.');
+    }
+  };
 
-var player = new Player(200,400,50);
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+
 var allEnemies = [];
+var player = new Hero(200, 390, 100);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
-        40: 'down'
-    };
+        40: 'down',
+
+        // just for debugging position
+        66: 'b',
+      };
+
     player.handleInput(allowedKeys[e.keyCode]);
-});
+  });
